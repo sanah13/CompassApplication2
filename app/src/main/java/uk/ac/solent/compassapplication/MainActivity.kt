a6d8b55145926a2c54afda9a5dc5a9745c01815a
sanah13 <Freaks123>
2019-06-13 14:48:58 +0100

    compass

--- app/src/main/java/uk/ac/solent/compassapplication/MainActivity.kt
+++ app/src/main/java/uk/ac/solent/compassapplication/MainActivity.kt
@@ -13,84 +13,71 @@ import android.view.animation.Animation
 import android.view.animation.RotateAnimation
 import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity  : AppCompatActivity(), SensorEventListener {
+    var currentDegree = 0.0f
+    lateinit var sensorManager: SensorManager
+    lateinit var image: ImageView
+    lateinit var accelerometer: Sensor
+    lateinit var magnetometer: Sensor
+
+    var Accel = FloatArray(5)
+    var Magnet = FloatArray(5)
+    var Accel2 = false
+    var Magnet2 = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image = R.id.img1 as ImageView
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
     }
-    class MainActivity  : AppCompatActivity(), SensorEventListener {
-        lateinit var sensorManager: SensorManager
-        lateinit var image: ImageView
-        lateinit var accelerometer: Sensor
-        lateinit var magnetometer: Sensor
+    override fun onResume() {
+        super.onResume()
 
-        var currentDegree = 0.0f
-        var lastAccelerometer = FloatArray(3)
-        var lastMagnetometer = FloatArray(3)
-        var lastAccelerometerSet = false
-        var lastMagnetometerSet = false
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-
-            image = R.id.img1 as ImageView
-            sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-            magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME)
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME)
+    }
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this, accelerometer)
+        sensorManager.unregisterListener(this, magnetometer)
+    }
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int){
+    }
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor === accelerometer) {
+            lowPass(event.values, Accel)
+            Accel2 = true
+        } else if (event.sensor === magnetometer) {
+            lowPass(event.values, Magnet)
+            Magnet2 = true
         }
+        if (Accel2 && Magnet2) {
+            val rotation = FloatArray(9)
+            if (SensorManager.getRotationMatrix(rotation, null, Accel, Magnet)) {
+                val orientation = FloatArray(3)
+                SensorManager.getOrientation(rotation, orientation)
+                val degree = (Math.toDegrees(orientation[0].toDouble()) + 360).toFloat() % 360
 
-        override fun onResume() {
-            super.onResume()
+                val rotateAnimation = RotateAnimation(
+                        currentDegree, -degree,
+                        Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f)
+                rotateAnimation.duration = 1000
+                rotateAnimation.fillAfter = true
 
-            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME)
-            sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME)
-        }
-
-        override fun onPause() {
-            super.onPause()
-            sensorManager.unregisterListener(this, accelerometer)
-            sensorManager.unregisterListener(this, magnetometer)
-        }
-
-        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
-
-        override fun onSensorChanged(event: SensorEvent) {
-            if (event.sensor === accelerometer) {
-                lowPass(event.values, lastAccelerometer)
-                lastAccelerometerSet = true
-            } else if (event.sensor === magnetometer) {
-                lowPass(event.values, lastMagnetometer)
-                lastMagnetometerSet = true
-            }
-
-            if (lastAccelerometerSet && lastMagnetometerSet) {
-                val r = FloatArray(9)
-                if (SensorManager.getRotationMatrix(r, null, lastAccelerometer, lastMagnetometer)) {
-                    val orientation = FloatArray(3)
-                    SensorManager.getOrientation(r, orientation)
-                    val degree = (Math.toDegrees(orientation[0].toDouble()) + 360).toFloat() % 360
-
-                    val rotateAnimation = RotateAnimation(
-                            currentDegree,
-                            -degree,
-                            Animation.RELATIVE_TO_SELF, 0.5f,
-                            Animation.RELATIVE_TO_SELF, 0.5f)
-                    rotateAnimation.duration = 1000
-                    rotateAnimation.fillAfter = true
-
-                    image.startAnimation(rotateAnimation)
-                    currentDegree = -degree
-                }
+                image.startAnimation(rotateAnimation)
+                currentDegree = -degree
             }
         }
-        fun lowPass(input: FloatArray, output: FloatArray) {
-            val alpha = 0.05f
-
-            for (i in input.indices) {
-                output[i] = output[i] + alpha * (input[i] - output[i])
-            }
+    }
+    fun lowPass(input: FloatArray, output: FloatArray) {
+        val alpha = 0.05f
+        for (i in input.indices) {
+            output[i] = output[i] + alpha * (input[i] - output[i])
         }
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -109,5 +96,4 @@ class MainActivity : AppCompatActivity() {
         }
         return true;
     }
-}
 }
